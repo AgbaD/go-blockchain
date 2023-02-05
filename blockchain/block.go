e0a8236ba85820b4b2bc0cc289fe4978baa46d5f
Damilare Agba <blankgodd33@gmail.com>
2023-02-05 15:23:40 +0100

    adding proof of work

---
+++ blockchain/block.go
@@ -0,0 +1,74 @@
+package blockchain
+
+// a blockchain contains multiple blocks
+//
+
+type BlockChain struct {
+	// array of pointers to blocks
+	// By making the first letter upper case
+	// it makes the field public
+	Blocks []*Block
+}
+
+// a struct (a data structure to represent to represent our block)
+type Block struct {
+	// block's hash = Data + prevHash
+	Hash []byte
+	//any type of data
+	Data []byte
+	// previous block's hash
+	PrevHash []byte
+	// the nonce value that meets the target for the block
+	Nonce int
+}
+
+// if the block struct is an object]
+// defining a function for the struct
+// i suppose
+// func (b *Block) DeriveHash() {
+// 	// a two dimensional slice of bytes
+// 	// takes in byte data and prevHash
+// 	// dummy way of hashing for now
+// 	//
+// 	// and the empty byte array
+// 	info := bytes.Join([][]byte{b.Data, b.PrevHash}, []byte{})
+// 	// hash the data
+// 	hash := sha256.Sum256(info)
+// 	b.Hash = hash[:]
+// }
+
+// outputs a pointer to a block
+func CreateBlock(data string, prevHash []byte) *Block {
+	// reference to a block
+	block := &Block{[]byte{}, []byte(data), prevHash, 0}
+	// using the struct fuction
+	// self defined
+	// block.DeriveHash()
+
+	// run the pow algorithm
+	pow := NewProof(block)
+	nonce, hash := pow.Run()
+
+	block.Hash = hash[:]
+	block.Nonce = nonce
+	return block
+}
+
+// any blockchain struct instance can use this function
+func (chain *BlockChain) AddBlock(data string) {
+	prevBlock := chain.Blocks[len(chain.Blocks)-1]
+	block := CreateBlock(data, prevBlock.Hash)
+	chain.Blocks = append(chain.Blocks, block)
+}
+
+// returns a pointer to a block
+func Genesis() *Block {
+	return CreateBlock("Genesis", []byte{})
+}
+
+// returns a pointer to the blockchain
+func InitBlockchain() *BlockChain {
+	// Takes an array of blocks pointers
+	// with a call to the genesis function as the first element of the array
+	return &BlockChain{[]*Block{Genesis()}}
+}
---
+++ blockchain/proof.go
@@ -0,0 +1,137 @@
+package blockchain
+
+import (
+	"bytes"
+	"crypto/sha256"
+	"encoding/binary"
+	"fmt"
+	"log"
+	"math"
+	"math/big"
+)
+
+// consensus algorithms
+// proof of; algorithms
+
+// forcing the network to do computational work to be able
+// to sign/add a block to the blockchain
+// so they have to be true to the proof the send
+// as it can be validated easily by other participants
+
+// Take the data from the block
+
+// create a counter (nonce) which starts at 0
+
+// create a hash of the data + the nonce
+
+// check the hash to see if it meets requirements
+
+// if not; repeat
+
+// Requirements
+// The First few bytes must contain 0s
+// For bitcoin, it was originally 20 and it changes from time to time
+
+// lets get to it
+
+// static
+// in a real blockchain, it has to increase overtime
+// due to increase in number of miners growing
+// and also computation power
+const Difficulty = 12
+
+type ProofOfWork struct {
+	Block  *Block
+	Target *big.Int
+}
+
+// produce a pointer to a POW
+func NewProof(b *Block) *ProofOfWork {
+	target := big.NewInt(1)
+	// it takes 256, which is the number of bytes in one hash
+	// the subtracts the difficulty from it
+	// then use the target to shift the number of bytes over by ...
+	// lsh = left shift
+	target.Lsh(target, uint(265-Difficulty))
+
+	pow := &ProofOfWork{b, target}
+	return pow
+}
+
+// create a method on a struct
+func (pow *ProofOfWork) InitData(nonce int) []byte {
+	data := bytes.Join(
+		[][]byte{
+			pow.Block.PrevHash,
+			pow.Block.Data,
+			ToHex(int64(nonce)),
+			ToHex(int64(Difficulty)),
+		},
+		[]byte{},
+	)
+	return data
+}
+
+func (pow *ProofOfWork) Run() (int, []byte) {
+	var intHash big.Int
+	var hash [32]byte
+
+	nonce := -0
+
+	for nonce < math.MaxInt64 {
+		// prepare data
+		data := pow.InitData(nonce)
+		// then hash to sha256
+		hash := sha256.Sum256(data)
+		fmt.Printf("\r%x", hash)
+
+		// then convert the hash to  a big int
+		intHash.SetBytes(hash[:])
+
+		// then compare the int to the target
+		if intHash.Cmp(pow.Target) == -1 {
+			break
+		} else {
+			nonce++
+		}
+	}
+	fmt.Println()
+	return nonce, hash[:]
+}
+
+
+// to validate a block
+// relatively simpler than getting the nonce
+func(pow *ProofOfWork) Validate() bool {
+	var intHash big.Int
+
+	data := pow.InitData(pow.Block.Nonce)
+	hash := sha256.Sum256(data)
+	fmt.Printf("\r%x", hash)
+	intHash.SetBytes(hash[:])
+
+	return intHash.Cmp(pow.Target) == -1
+}
+
+// we need to add the nonce and Difficulty to the InitData function; the
+// point is the find a value (nonce) for which when added to the
+// block data and hashed, it results in a hash that meets the
+// requirement placed. Then the miner sends the block and the
+// nonce once they are done so any other person
+// can take the previous block hash, the proposed block data
+// and the nonce and vefiry if it meets the requirements
+//
+//
+
+// a utility function
+func ToHex(num int64) []byte {
+	// creates a new bytes buffer
+	buff := new(bytes.Buffer)
+	// Takes the number and decode into bytes
+	// BigEndian defines how we want the bytes to be organised
+	err := binary.Write(buff, binary.BigEndian, num)
+	if err != nil {
+		log.Panic(err)
+	}
+	return buff.Bytes()
+}
